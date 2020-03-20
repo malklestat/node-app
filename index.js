a8e83cb97d3da47e172d657a9b90c43273dff397
malklestat <malklestat@hotmail.com>
2020-03-20 16:30:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Deploy feito com PM2');
 });
 
 app.listen(3000, function () {
